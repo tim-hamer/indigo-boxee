53542577808dc4c9a2c6f74d7412489023329f3e
Tim Hamer <tim@arcaexterna.com>
2014-06-03 13:23:39 -0500

    initial commit

---
+++ Contents/Server Plugin/plugin.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python # -*- coding: utf-8 -*-
+####################
+
+import jsonrpclib, sys, os, pprint, urllib2
+
+class Plugin(indigo.PluginBase):
+
+############## --- Indigo Plugin Methods --- ##############
+
+    def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
+        indigo.PluginBase.__init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
+        self.debug = True
+
+        self.commands = {
+            'up': 'SendKey(270)',
+            'down': 'SendKey(271)',
+            'left': 'SendKey(272)',
+            'right': 'SendKey(2703)',
+            'select': 'SendKey(256)',
+            'back': 'SendKey(275)',
+            'pause': 'Pause()',
+            'get_percentage':'GetPercentage'
+        }
+
+	def __del__(self):
+		indigo.PluginBase.__del__(self)
+
+	def startup(self):
+		self.debugLog(u"Boxee Plugin Started")
+
+	def shutdown(self):
+		self.debugLog(u"Boxee Plugin Stopping...")
+
+############## --- Action Methods --- ##############
+
+    def sendCommand(self, action, device):
+        ip = device.pluginProps['ipaddress']
+        cmd = self.commands[action.props['command']]
+        url = 'http://%s:8800/xbmcCmds/xbmcHttp?command=%s' % (ip, cmd)
+        urllib2.urlopen(url)
---
+++ README.md
@@ -0,0 +1,5 @@
+indigo-boxee
+===========
+
+Control a Boxee Box using Indigo. Very trivial implementation only sends
+basic navigation keys and pause command.
